c0b501927228a47d6418b6788f9fa6cc1177bc29
Trevor Richardson <trevor@gaincompliance.com>
2023-07-27 11:15:54 -0500

    module 4 refactoring

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -3,30 +3,26 @@ import {checkGuess} from '../../game-helpers';
 import {range} from '../../utils';
 
 
-function GuessLetter(props) {
-  return <span className={`cell ${props.answerStyle}`}>
-          {props.characterResult.letter}
+function GuessLetter({answerStyle, letter}) {
+  return <span className={`cell ${answerStyle}`}>
+          {letter}
         </span>;
 }
 
 function Guess({guess, answer}) {
-  if (guess === "") {
-    return <p className="guess">
-      {range(0, 5).map((c, index) => <span key={index} className="cell"></span>)}
-    </p>;
-  }
+  // if (guess === "") {
+  //   return <p className="guess">
+  //     {range(0, 5).map((c, index) => <span key={index} className="cell"></span>)}
+  //   </p>;
+  // }
 
   const guessResults = checkGuess(guess, answer);
+  console.log(guessResults);
 
   return <p className="guess">
-    {guessResults.map((characterResult, index) => {
-      if (guessResults[index].status === "correct") {
-        return <GuessLetter key={index} answerStyle="correct" characterResult={characterResult}/>
-      } else if (guessResults[index].status === "misplaced") {
-        return <GuessLetter key={index} answerStyle="misplaced" characterResult={characterResult} />
-      }
-
-      return <GuessLetter key={index} characterResult={characterResult} />
+    {range(0,5).map((number) => {
+      const characterResult = guessResults ? guessResults[number].letter : "";
+      return <GuessLetter key={number} answerStyle={guessResults ? guessResults[number].status : ""} letter={characterResult}/>
     })}
   </p>;
 }
